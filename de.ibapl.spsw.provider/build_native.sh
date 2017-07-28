1691046274336044dfc3511340be89e8c7461ee9
Arne Plöse <aploese@tamar.lan>
2017-07-28 20:28:35 +0200

    fixed scripts

--- de.ibapl.spsw.provider/build_native.sh
+++ de.ibapl.spsw.provider/build_native.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mvn clean compile -P linux-amd64
-mvn clean compile -P linux-i386
-mvn clean compile -P arm-linux-gnueabihf-gcc
-mvn clean compile -P mipsel-linux-gnu-gcc
-mvn clean compile -P i686-w64-mingw32-gcc
+mvn clean compile -P linux-amd64 && \
+mvn clean compile -P linux-i386 && \
+mvn clean compile -P arm-linux-gnueabihf-gcc && \
+mvn clean compile -P mipsel-linux-gnu-gcc && \
+mvn clean compile -P i686-w64-mingw32-gcc && \
 mvn clean compile -P x86_64-w64-mingw32-gcc
--- de.ibapl.spsw.provider/cross-compile-setup-debian.sh
+++ de.ibapl.spsw.provider/cross-compile-setup-debian.sh
@@ -2,16 +2,18 @@
 apt install gcc-mingw-w64
 
 dpkg --add-architecture amd64
-apt install libc6-dev:amd64 libgcc-6-dev:amd64
+apt update
+apt install libc6-dev:amd64
 apt install lib64gcc-6-dev
 
 dpkg --add-architecture i386
-apt install libc6-dev:i386 libgcc-6-dev:i386
+apt update
+apt install libc6-dev:i386
 apt install lib32gcc-6-dev
 
-dpkg --add-architecture armhf
-apt install gcc-arm-linux-gnueabihf libc6-dev:armhf libgcc-6-dev:armhf
+#dpkg --add-architecture armhf
+apt install gcc-arm-linux-gnueabihf
 
-dpkg --add-architecture mipsel
-apt install gcc-mipsel-linux-gnu libc6-dev:mipsel libgcc-6-dev:mipsel
+#dpkg --add-architecture mipsel
+apt install gcc-mipsel-linux-gnu
 
