da4129055e3f400806f7c2a9c427a20cdf60dbc1
Arne Plöse <aploese@tamar.lan>
2017-07-28 21:25:26 +0200

    shrink the lib by stripping

--- de.ibapl.spsw.provider/build_native.sh
+++ de.ibapl.spsw.provider/build_native.sh
@@ -4,4 +4,5 @@ mvn clean compile -P linux-i386 && \
 mvn clean compile -P arm-linux-gnueabihf-gcc && \
 mvn clean compile -P mipsel-linux-gnu-gcc && \
 mvn clean compile -P i686-w64-mingw32-gcc && \
-mvn clean compile -P x86_64-w64-mingw32-gcc
+mvn clean compile -P x86_64-w64-mingw32-gcc && \
+mvn clean install
