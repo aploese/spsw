5d120314d8fcf504d301a7e23d5d7b526926f9c2
aploese <aploese@gmx.de>
2018-02-20 11:56:06 +0100

    Output enhancements

--- de.ibapl.spsw.provider/src/test/java/de/ibapl/spsw/tests/PrintPortCapabillitiesMain.java
+++ de.ibapl.spsw.provider/src/test/java/de/ibapl/spsw/tests/PrintPortCapabillitiesMain.java
@@ -6,10 +6,14 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.logging.Level;
 
+import javax.sql.rowset.serial.SerialException;
+import javax.swing.text.NavigationFilter;
+
 import de.ibapl.spsw.api.Baudrate;
 import de.ibapl.spsw.api.DataBits;
 import de.ibapl.spsw.api.FlowControl;
 import de.ibapl.spsw.api.Parity;
+import de.ibapl.spsw.api.SerialPortException;
 import de.ibapl.spsw.api.SerialPortSocket;
 import de.ibapl.spsw.api.StopBits;
 import de.ibapl.spsw.provider.SerialPortSocketFactoryImpl;
@@ -17,11 +21,10 @@ import de.ibapl.spsw.provider.SerialPortSocketFactoryImpl;
 public class PrintPortCapabillitiesMain {
 
 	public static void main(String[] args) throws Exception {
-		SerialPortSocket serialPortSocket = SerialPortSocketFactoryImpl.singleton()
-				.createSerialPortSocket("/dev/ttyUSB0");
+		SerialPortSocket serialPortSocket = SerialPortSocketFactoryImpl.singleton().createSerialPortSocket("COM6");
 		serialPortSocket.openRaw();
-        System.out.println("Use device: " + serialPortSocket.getPortName());
-        System.out.println(String.format("%-20s%-20s%-20s%-20s", "Baudrate", "DataBits", "StopBits", "Parity"));
+		System.out.println("Use device: " + serialPortSocket.getPortName());
+		System.out.println(String.format("%-20s%-20s%-20s%-20s", "Baudrate", "DataBits", "StopBits", "Parity"));
 
 		for (Baudrate br : Baudrate.values()) {
 			for (DataBits db : DataBits.values()) {
@@ -31,32 +34,53 @@ public class PrintPortCapabillitiesMain {
 							serialPortSocket.setBaudrate(br);
 						} catch (IllegalArgumentException e) {
 							System.err.println("Cant set Baudrate to: " + br);
+						} catch (SerialPortException spe) {
+							System.err.println("Error: set Baudrate to: " + br);
+							System.err.println(spe);
 						}
 						try {
 							serialPortSocket.setDataBits(db);
 						} catch (IllegalArgumentException e) {
 							System.err.println("Cant set DataBits to: " + db);
+						} catch (SerialPortException spe) {
+							System.err.println("Error: set DataBits to: " + db);
+							System.err.println(spe);
 						}
 						try {
 							serialPortSocket.setStopBits(sb);
 						} catch (IllegalArgumentException e) {
 							System.err.println("Cant set StopBits to: " + sb);
+						} catch (SerialPortException spe) {
+							System.err.println("Error: set StopBits to: " + sb);
+							System.err.println(spe);
 						}
 						try {
 							serialPortSocket.setParity(p);
 						} catch (IllegalArgumentException e) {
 							System.err.println("Cant set Parity to: " + p);
+						} catch (SerialPortException spe) {
+							System.err.println("Error: set Parity to: " + p);
+							System.err.println(spe);
+							Thread.sleep(10);
 						}
 						printPort(serialPortSocket);
 					}
+				}
+				// @5 Databis 1 or 1.5 Stopbits otherwise only 1 or 2 Stopbits...
+				try {
+					serialPortSocket.setStopBits(StopBits.SB_1);
+				} catch (SerialPortException spe) {
+					System.err.println("Error: set StopBits to: " + StopBits.SB_1);
+					System.err.println(spe);
 				}
 			}
 		}
 		serialPortSocket.close();
 	}
 
-	    private static void printPort(SerialPortSocket sPort) throws IOException {
-	        System.out.println(String.format("%-20d%-20d%-20f%-20s", sPort.getBaudrate().value, sPort.getDatatBits().value, sPort.getStopBits().value, sPort.getParity().name()));
+	private static void printPort(SerialPortSocket sPort) throws IOException {
+		System.out.println(String.format("%-20d%-20d%-20f%-20s", sPort.getBaudrate().value, sPort.getDatatBits().value,
+				sPort.getStopBits().value, sPort.getParity().name()));
 	}
 
 }
