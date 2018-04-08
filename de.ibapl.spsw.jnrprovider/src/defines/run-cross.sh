db432e4dcfa7d09f2ef6889458301120ab11eb61
Arne Plöse <aploese@gmx.de>
2018-04-08 07:29:15 +0200

    added castxml for gererating xml compiler output

--- de.ibapl.spsw.jnrprovider/src/defines/run-cross.sh
+++ de.ibapl.spsw.jnrprovider/src/defines/run-cross.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for d in "aarch64-linux-gnu" "arm-linux-gnueabi" "arm-linux-gnueabihf" "mips-linux-gnu" "mips64-linux-gnuabi64" "mips64el-linux-gnuabi64" "mipsel-linux-gnu" "x86_64-linux-gnu"; do
+for d in "aarch64-linux-gnu" "arm-linux-gnueabi" "arm-linux-gnueabihf" "mips-linux-gnu" "mips64-linux-gnuabi64" "mips64el-linux-gnuabi64" "mipsel-linux-gnu" "x86_64-linux-gnu" "i686-linux-gnu"; do
 ./run-preprocessor.sh $d;
 done
 
--- de.ibapl.spsw.jnrprovider/src/defines/run-preprocessor.sh
+++ de.ibapl.spsw.jnrprovider/src/defines/run-preprocessor.sh
@@ -11,8 +11,13 @@ done;
 
 echo $GCC
 echo $MULTIARCH
-for d in "termios.h" "sys/ioctl.h" "poll.h" "fcntl.h" "stdio.h" "sys/eventfd.h"; do
-echo "#include <$d>" | $GCC -dD -dI -E -xc - > $MULTIARCH/$d.txt
+
+echo "" | $GCC -dD -dI -E -xc - > $MULTIARCH/empty.txt
+
+for d in "termios" "sys/ioctl" "poll" "fcntl" "stdio" "sys/eventfd" "elf"; do
+  echo "#include <$d.h>" | $GCC -dD -dI -E -xc - > $MULTIARCH/$d-prepocessor.txt
+  echo "#include <$d.h>" | $GCC -dM -E -xc - > $MULTIARCH/$d.defines
+  echo "#include <$d.h>" | castxml --castxml-cc-gnu-c $GCC --castxml-output=1 -x c -o $MULTIARCH/$d.xml -
 done
 
 
