458b28d8e80687089388af5e76723e5ca369e1fd
Arne Plöse <aploese@gmx.de>
2019-10-07 13:09:37 +0200

    Cross compile

--- de.ibapl.spsw.jniprovider/build-cross-linux.sh
+++ de.ibapl.spsw.jniprovider/build-cross-linux.sh
@@ -7,6 +7,12 @@ mvn compile
 # "x86_64-linux-gnu"
 # "i386-linux-gnu"
 
+# here the compiler produces garbage
+# "mipsisa32r6el-linux-gnu"\
+# "mipsisa32r6-linux-gnu"\
+# "mipsisa64r6el-linux-gnuabi64"\
+# "mipsisa64r6-linux-gnuabi64"
+
 for d in\
  "aarch64-linux-gnu"\
  "arm-linux-gnueabi"\
